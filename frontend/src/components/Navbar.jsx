ba86e27dd86444821e60f0da2965d4c0c755dfc0
Dev <113640546+Dev2kod@users.noreply.github.com>
2025-11-26 09:32:01 +0000

    changes to run project

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
-import Logo from './logo'
-import DarkButton from './darkButton'
+import Logo from './Logo'
+import DarkButton from './DarkButton'
 
 const Navbar = () => {
   return (
