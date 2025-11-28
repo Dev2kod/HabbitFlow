84b30ba31d6868ab0ac086187a8266da1a653ab1
Dev <113640546+Dev2kod@users.noreply.github.com>
2025-11-28 16:55:58 +0530

    Refactor routing and update Home component import

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,17 @@
-import Home from './pages/Home.jsx'
+import Home from './pages/home.jsx'
 import "./App.css"
+import {Route,Routes} from "react-router-dom"
 import Navbar from './components/Navbar.jsx';
+import Profile from './pages/Profile.jsx';
 function App() {
 
   return (
     <><div style={{display:"flex",flexDirection:"column"}}>
     <Navbar style={{position:"fixed", top:0,width:"100%",alignItems:"center"}}/>
-    <Home></Home>
+    <Routes>
+    <Route path="" element={<Home/>}/>
+    <Route path="/profile" element={<Profile/>}/>
+    </Routes>
     </div>
     </>
   )
